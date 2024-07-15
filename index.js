da119a4fc8b8d7e58b5e334c5228b42bea1cf9ae
rahul <rahul.rajput909090.com>
2024-07-15 12:20:27 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const express = require("express")
+const express = require("express");
 const { Server } = require("socket.io");
-var http = require('http');
-const cors = require("cors")
+var http = require("http");
+const cors = require("cors");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(
+  cors({
+    origin: "https://macommunity.netlify.app", // Replace with your Netlify app URL
+    methods: ["GET", "POST"],
+  })
+);
 
 var server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "https://macommunity.netlify.app", // Replace with your Netlify app URL
+    methods: ["GET", "POST"],
+  },
 });
 
 app.get("/", (req, res) => {
-    res.send("Socket server strated");
-    res.end()
-})
-
-io.on("connection", (socket) => {
-    console.log(socket.id)
-
-    socket.on("joinRoom", room => {
-        socket.join(room)
-    })
-
-    socket.on("newMessage", ({ newMessage, room }) => {
-        io.in(room).emit("getLatestMessage", newMessage)
-    })
-
+  res.send("Socket server started");
+  res.end();
 });
 
-const port = process.env.PORT || 8000
+io.on("connection", (socket) => {
+  console.log(socket.id);
 
-server.listen(port, console.log(`App started at port ${port}`))
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("newMessage", ({ newMessage, room }) => {
+    io.in(room).emit("getLatestMessage", newMessage);
+  });
+});
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`App started at port ${port}`);
+});
