b133e588c726cf4f0df05f517920c4a912161143
rahul <rahul.rajput909090.com>
2024-07-15 15:43:31 +0530

    Fourth commit

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-require("dotenv").config();
-
 const express = require("express");
 const { Server } = require("socket.io");
-const http = require("http");
+var http = require("http");
 const cors = require("cors");
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors());
 
-const server = http.createServer(app);
+var server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "https://macommunity.netlify.app",
     methods: ["GET", "POST"],
   },
 });
 
 app.get("/", (req, res) => {
-  res.send("Socket server started");
+  res.send("Socket server strated");
   res.end();
 });
 
@@ -41,6 +34,4 @@ io.on("connection", (socket) => {
 
 const port = process.env.PORT || 8000;
 
-server.listen(port, () => {
-  console.log(`App started at port ${port}`);
-});
+server.listen(port, console.log(`App started at port ${port}`));
